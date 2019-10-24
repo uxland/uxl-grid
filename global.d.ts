57609d801945b918a75d29da04fd83129f8bb021
Adrián Guillén Bermúdez <aguillen@uxland.es>
2019-10-24 16:11:13 +0200

    fix: text

--- global.d.ts
+++
@@ -1,9 +0,0 @@
-declare module '*.css' {
-   const exports: { [exportName: string]: string };
-    export = exports;
-  }
-
-declare module '*.scss' {
-  const content: {[className: string]: string};
-  export = content;
-}
