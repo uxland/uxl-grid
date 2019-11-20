2b1b3980ef8705a9f5985cb39590b7dcfc8ae65b
Feliu Viaplana <info@altersites.com>
2019-11-20 09:05:59 +0100

    fix: ux & ui

--- src/components/uxl-grid/template.ts
+++ src/components/uxl-grid/template.ts
@@ -10,7 +10,7 @@ export const template = (props: UxlGrid) => html`
   ${props.isFormatGrid ?
         html`
             <format-grid .source="${props.source}" .columns="${props.columns}" part="format-grid"
-            exportparts="header, header__cell-1, header__cell-2, header__cell-3, header__cell-4, header__cell-5, header__cell-6, header__cell-7, content, content__row, content__cell, content__cell-1, content__cell-2, content__cell-3, content__cell-4, content__cell-5, content__cell-6"></format-grid>` :
+            exportparts="header, header__cell-1, header__cell-2, header__cell-3, header__cell-4, header__cell-5, header__cell-6, header__cell-7, content, content__row, content__cell, content__cell-1, content__cell-2, content__cell-3, content__cell-4, content__cell-5, content__cell-6, content__cell-7"></format-grid>` :
 		html `
 		    <format-table .source="${props.source}" .columns="${props.columns}"></format-table>
 		`}
